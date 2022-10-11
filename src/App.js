1922eaa8c1b28e83d59ced12b2833c58e44a4176
Nicholas Lehman <nick.lehman@icims.com>
2022-10-11 11:24:55 -0400

    Improve semantic tag usage

--- src/App.js
+++ src/App.js
@@ -5,12 +5,19 @@ import Clock from "react-live-clock";
 export default function App() {
   return (
     <div className="App">
-      <Clock format="h:mm:ss" interval={1000} ticking={true} />
-      <select>
-        <option>
-          {new window.Intl.DateTimeFormat().resolvedOptions().timeZone}{" "}
-        </option>
-      </select>
+      <fieldset class="timing">
+        <legend>Timer Display</legend>
+        <Clock format="h:mm:ss" interval={1000} ticking={true} />
+        <select>
+          <option>
+            {new window
+              .Intl
+              .DateTimeFormat()
+              .resolvedOptions()
+              .timeZone}
+          </option>
+        </select>
+      </fieldset>
     </div>
   );
 }
